c2adf345b46f4f7eab503ad4ef9928dba8f57f16
SilvaRuan16 <ruan.gustavo.soares16@gmail.com>
2025-06-10 20:44:13 -0300

    First commit

---
+++ src/test/java/capital/dijkstra/DijkstraApplicationTests.java
@@ -0,0 +1,13 @@
+package capital.dijkstra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DijkstraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
